0e6f60c4adb9c965e89b1aa5fe3b10f3949925f0
Victor Shaviya <vs610642@digicelgroup.com>
2022-07-02 15:54:32 -0500

    Create the function for changing the background and call the the function upon clicks

--- assets/logic.js
+++ assets/logic.js
@@ -1,4 +1,11 @@
+// swapping the images function
 function imgSlider(anything) {
   document.querySelector('.pepsi').src = anything;
   // console.log("Yes, the function runs")
+}
+
+// background change function
+function changeBgColor(color) {
+  const sec = document.querySelector('.sec');
+  sec.style.background = color;
 }
